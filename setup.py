e0ae12fe64467fbbb39cf2a8cbfca4c82e65cfa4
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-04-29 17:03:19 -0400

    Use pyproject.toml for setuptools_scm

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name="test",
     description="",
-    use_scm_version={
-        "version_scheme": "post-release",
-        "relative_to": __file__,
-        "root": "..",
-    },
-    setup_requires=["setuptools_scm"],
+    # use_scm_version={
+    #     "version_scheme": "post-release",
+    #     "relative_to": __file__,
+    #     "root": "..",
+    # },
+    # setup_requires=["setuptools_scm"],
     packages=find_packages(),
 )
